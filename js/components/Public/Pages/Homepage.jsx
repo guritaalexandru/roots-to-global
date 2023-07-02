730e3aed7c37267210a26852744e48a857d92c15
guritaalexandru <a_gurita@yahoo.com>
2023-07-02 15:32:29 +0300

    Initial setups

---
+++ js/components/Public/Pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Homepage(props) {
+	return (
+		<div>This is the Homepage</div>
+	);
+}
+
+export default Homepage;
---
+++ js/components/Public/Sections/InitialSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InitialSection(props) {
+	return (
+		<div></div>
+	);
+}
+
+export default InitialSection;
