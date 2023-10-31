c8b0caec8bec294dae632372f4e12eccdc212f01
guritaalexandru <a_gurita@yahoo.com>
2023-10-31 19:40:00 +0200

    Continue integration 2 test

--- app/api/payments/webhook/route.js
+++ app/api/payments/webhook/route.js
@@ -1,15 +1,19 @@
 import { NextResponse, } from 'next/server';
+import {postRegistration,} from '@/js/utils/serverCalls';
 
 export async function POST(request) {
 	const body = await request.json();
 	const paymentId = body.id;
 	console.log('MOLLIE paymentId', paymentId);
+	const dbPostResponse = await postRegistration(body);
 
 	const { createMollieClient, } = require('@mollie/api-client');
 	const mollieClient = createMollieClient({ apiKey: process.env.MOLLIE_API_KEY, });
 
 	const payment = await mollieClient.payments.get(paymentId);
 	console.log('MOLLIE payment', payment);
+
+	const dbPostResponse2 = await postRegistration(payment);
 
 	return NextResponse.json({ message: 'Success', }, { status: 200, });
 }
