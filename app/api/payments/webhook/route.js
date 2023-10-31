8d8caab919e49e87650025481188e3c41a753b9f
guritaalexandru <a_gurita@yahoo.com>
2023-10-31 19:26:42 +0200

    Continue integration

--- app/api/payments/webhook/route.js
+++ app/api/payments/webhook/route.js
@@ -5,5 +5,11 @@ export async function POST(request) {
 	const paymentId = body.id;
 	console.log('MOLLIE paymentId', paymentId);
 
+	const { createMollieClient, } = require('@mollie/api-client');
+	const mollieClient = createMollieClient({ apiKey: process.env.MOLLIE_API_KEY, });
+
+	const payment = await mollieClient.payments.get(paymentId);
+	console.log('MOLLIE payment', payment);
+
 	return NextResponse.json({ message: 'Success', }, { status: 200, });
 }
